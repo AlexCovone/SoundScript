8473fe0b7cc1e5c2e087173fcf7dffc6a896257d
Alex Covone <alexcovone@gmail.com>
2023-05-04 22:22:50 -0400

    working tailwind build

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  content: ["./views/**/*.ejs"],
+  theme: {
+    screens: {
+      xs: '330px',
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px'
+    },
+    extend: {
+    },
+  },
+  plugins: [],
+  daisyui: {
+    themes: [],
+  },
+}
