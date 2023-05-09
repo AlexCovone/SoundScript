939d16a2af6a513939e2c71f3bbb134138903a06
Alex Covone <alexcovone@gmail.com>
2023-05-08 23:13:20 -0400

    styles: Add landing page and reformat selects for source and target language

--- public/main.js
+++ public/main.js
@@ -3,41 +3,28 @@ var phraseDiv;
 var startRecognizeOnceAsyncButton;
 
 // subscription key and region for speech services.
-var subscriptionKey,
-  serviceRegion,
-  languageTargetOptions,
-  languageSourceOptions;
+var subscriptionKey, serviceRegion, languageTargetOptions, languageSourceOptions;
 var SpeechSDK;
 var recognizer;
 
-document.addEventListener("DOMContentLoaded", function () {
-  startRecognizeOnceAsyncButton = document.getElementById(
-    "startRecognizeOnceAsyncButton"
-  );
-  subscriptionKey = document.getElementById("subscriptionKey");
-  serviceRegion = document.getElementById("serviceRegion");
-  languageTargetOptions = document.getElementById("languageTargetOptions");
-  languageSourceOptions = document.getElementById("languageSourceOptions");
-  phraseDiv = document.getElementById("phraseDiv");
+document.addEventListener('DOMContentLoaded', function () {
+  startRecognizeOnceAsyncButton = document.getElementById('startRecognizeOnceAsyncButton');
+  subscriptionKey = document.getElementById('subscriptionKey');
+  serviceRegion = document.getElementById('serviceRegion');
+  languageTargetOptions = document.getElementById('languageTargetOptions');
+  languageSourceOptions = document.getElementById('languageSourceOptions');
+  phraseDiv = document.getElementById('phraseDiv');
 
-  startRecognizeOnceAsyncButton.addEventListener("click", function () {
+  startRecognizeOnceAsyncButton.addEventListener('click', function () {
     startRecognizeOnceAsyncButton.disabled = true;
-    phraseDiv.innerHTML = "";
+    phraseDiv.innerHTML = '';
 
-    if (
-      subscriptionKey.value === "" ||
-      subscriptionKey.value === "subscription"
-    ) {
-      alert(
-        "Please enter your Microsoft Cognitive Services Speech subscription key!"
-      );
+    if (subscriptionKey.value === '' || subscriptionKey.value === 'subscription') {
+      alert('Please enter your Microsoft Cognitive Services Speech subscription key!');
       startRecognizeOnceAsyncButton.disabled = false;
       return;
     }
-    var speechConfig = SpeechSDK.SpeechTranslationConfig.fromSubscription(
-      subscriptionKey.value,
-      serviceRegion.value
-    );
+    var speechConfig = SpeechSDK.SpeechTranslationConfig.fromSubscription(subscriptionKey.value, serviceRegion.value);
 
     speechConfig.speechRecognitionLanguage = languageSourceOptions.value;
     let language = languageTargetOptions.value;
@@ -71,20 +58,20 @@ document.addEventListener("DOMContentLoaded", function () {
     SpeechSDK = window.SpeechSDK;
     startRecognizeOnceAsyncButton.disabled = false;
 
-    document.getElementById("content").style.display = "block";
-    document.getElementById("warning").style.display = "none";
+    document.getElementById('content').style.display = 'block';
+    document.getElementById('warning').style.display = 'none';
   }
 });
 
-document.querySelector("#getText").addEventListener("click", textToSpeech);
+document.querySelector('#getText').addEventListener('click', textToSpeech);
 
 async function textToSpeech() {
   try {
-    const contentFromTextArea = document.querySelector("#phraseDiv").value;
+    const contentFromTextArea = document.querySelector('#phraseDiv').value;
 
-    const response = await fetch("textToSpeech", {
-      method: "post",
-      headers: { "Content-Type": "application/json" },
+    const response = await fetch('textToSpeech', {
+      method: 'post',
+      headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
         sendText: contentFromTextArea,
       }),
