6bfa953868fc83d523bbe6b84989b410a09973f2
Alex Covone <alexcovone@gmail.com>
2023-05-18 20:18:33 -0400

    feat: add userHistory/profile page. Connect routes with ensureAuth middleware

--- controllers/translations.js
+++ controllers/translations.js
@@ -1,1 +1,11 @@
 const Translation = require("../models/Translation");
+
+module.exports = {
+    getProfile: async (req, res) => {
+      try {
+        res.render("userHistory.ejs", {user: req.user });
+      } catch (err) {
+        console.log(err);
+      }
+    },
+}
--- routes/main.js
+++ routes/main.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/auth');
-// const eventsController = require('../controllers/events');
+const translationController = require('../controllers/translations')
 const { ensureAuth } = require('../middleware/auth');
 
-// router.get('/profile', ensureAuth, eventsController.getProfile);
+
 router.get('/login', authController.getLogin);
 router.post('/login', authController.postLogin);
 router.get('/logout', authController.logout);
 router.get('/signup', authController.getSignup);
 router.post('/signup', authController.postSignup);
+router.get("/profile", ensureAuth, translationController.getProfile);
 
 module.exports = router;
