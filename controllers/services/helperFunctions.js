7fb5af88a675eb1de09a52ed73be84b2db8e2f51
Alex Covone <alexcovone@gmail.com>
2023-05-20 18:45:30 -0400

    perf: abstract hashmaps into helper function folder

--- controllers/services/helperFunctions.js
+++ controllers/services/helperFunctions.js
@@ -3,5 +3,49 @@ module.exports = {
         const date = new Date(Date.now())
         const format = { month: 'long', day: 'numeric', year: 'numeric', timeZone: 'America/New_York' }
         return date.toLocaleDateString('en-US', format)        
+    },
+
+    mapSourceLanguage: (languageCode) => {
+        const sourceLanguageMap = {
+            'en-US': 'English',
+            'de-DE': 'German',
+            'ar-EG': 'Arabic',
+            'es-ES': 'Spanish',
+            'fi-FI': 'Finnish',
+            'fr-FR': 'French',
+            'hi-IN': 'Hindi',
+            'it-IT': 'Italian',
+            'ja-JP': 'Japanese',
+            'ko-KR': 'Korean',
+            'pl-PL': 'Polish',
+            'pt-BR': 'Portugese',
+            'ru-RU': 'Russian',
+            'sv-SE': 'Swedish',
+            'zh-Hans': 'Chinese'
+          };
+
+        return sourceLanguageMap[languageCode]
+    },
+
+    mapTargetLanguage: (languageCode) => {
+        const targetLanguageMap = {
+            'en': 'English',
+            'de': 'German',
+            'ar': 'Arabic',
+            'es': 'Spanish',
+            'fi': 'Finnish',
+            'fr': 'French',
+            'hi': 'Hindi',
+            'it': 'Italian',
+            'ja': 'Japanese',
+            'ko': 'Korean',
+            'pl': 'Polish',
+            'pt': 'Portugese',
+            'ru': 'Russian',
+            'sv': 'Swedish',
+            'zh-Hans': 'Chinese'
+          };
+
+        return targetLanguageMap[languageCode]
     }
 }
--- controllers/translations.js
+++ controllers/translations.js
@@ -1,62 +1,25 @@
 const Translation = require('../models/Translation');
-const { getFormattedCurrentDate } = require('../controllers/services/helperFunctions');
-
+const { getFormattedCurrentDate, mapSourceLanguage, mapTargetLanguage } = require('../controllers/services/helperFunctions');
 
 module.exports = {
   getProfile: async (req, res) => {
     try {
-      const translations = await Translation.find({ user: req.user.id}).sort({ createdAt: 'desc' })
+      const translations = await Translation.find({ user: req.user.id }).sort({ createdAt: 'desc' });
 
-      console.log(translations)
+      console.log(translations);
 
-      res.render('userHistory.ejs', {translations, user: req.user });
+      res.render('userHistory.ejs', { translations, user: req.user });
     } catch (err) {
       console.log(err);
     }
   },
-  
+
   saveTranslation: async (req, res) => {
     try {
-      const sourceLanguageMap = {
-        'en-US' : 'English',
-        'de-DE' : 'German',
-        'ar-EG' : 'Arabic',
-        'es-ES' : 'Spanish',
-        'fi-FI' : 'Finnish',
-        'fr-FR' : 'French',
-        'hi-IN' : 'Hindi',
-        'it-IT' : 'Italian',
-        'ja-JP' : 'Japanese',
-        'ko-KR' : 'Korean',
-        'pl-PL' : 'Polish',
-        'pt-BR' : 'Portugese',
-        'ru-RU' : 'Russian',
-        'sv-SE' : 'Swedish',
-        'zh-Hans' : 'Chinese'
-      }
-
-      const targetLanguageMap = {
-        'en' : 'English',
-        'de' : 'German',
-        'ar' : 'Arabic',
-        'es' : 'Spanish',
-        'fi' : 'Finnish',
-        'fr' : 'French',
-        'hi' : 'Hindi',
-        'it' : 'Italian',
-        'ja' : 'Japanese',
-        'ko' : 'Korean',
-        'pl' : 'Polish',
-        'pt' : 'Portugese',
-        'ru' : 'Russian',
-        'sv' : 'Swedish',
-        'zh-Hans' : 'Chinese'
-      }
-
       await Translation.create({
         translation: req.body.translation,
-        sourceLanguage: sourceLanguageMap[req.body.sourceLanguage],
-        targetLanguage: targetLanguageMap[req.body.targetLanguage],
+        sourceLanguage: mapSourceLanguage(req.body.sourceLanguage),
+        targetLanguage: mapTargetLanguage(req.body.targetLanguage),
         formattedDate: getFormattedCurrentDate(),
         user: req.user.id,
       });
