9b662226b90f007fbc47263cc5623a9eaf724ee1
Alex Covone <alexcovone@gmail.com>
2023-05-20 13:58:31 -0400

    feat: create translation var to find all translations by user  on getProfile method when visting profile

--- controllers/translations.js
+++ controllers/translations.js
@@ -3,17 +3,16 @@ const Translation = require('../models/Translation');
 module.exports = {
   getProfile: async (req, res) => {
     try {
-      // const user = await User.findById(req.user._id);
+      const translations = await Translation.find({ user: req.user.id}).sort({ createdAt: 'desc' })
+      console.log(translations)
 
-      res.render('userHistory.ejs', { user: req.user });
+      res.render('userHistory.ejs', {user: req.user });
     } catch (err) {
       console.log(err);
     }
   },
   saveTranslation: async (req, res) => {
     try {
-      console.log(req.body);
-
       await Translation.create({
         translation: req.body.translation,
         sourceLanguage: req.body.sourceLanguage,
