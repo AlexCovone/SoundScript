1d3cb2685298d3db64b85acc922eb3330910b8d4
Alex Covone <alexcovone@gmail.com>
2023-07-22 17:33:14 -0400

    refactor: comment sampleTranslation collection out

--- app.js
+++ app.js
@@ -71,8 +71,8 @@ app.get('/', async (req, res) => {
   console.log(message);
 
   // Select 7 random documents in Translation collection
-  const sampleCollection = db.collection('exampleTranslations');
-  const translations = await sampleCollection.aggregate([{ $sample: { size: 7 } }]).toArray();
+  // const sampleCollection = db.collection('exampleTranslations');
+  // const translations = await sampleCollection.aggregate([{ $sample: { size: 7 } }]).toArray();
 
   res.render('index.ejs', { date, currentYear: year, subscriptionKey, serviceRegion, message, translations, user: req.user });
 });
