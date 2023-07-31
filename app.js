af69039fbf81ddf4d211a8c747f035c9fabd7d63
Alex Covone <alexcovone@gmail.com>
2023-07-31 14:27:29 -0400

    refactor: move sampleTranslations into sampleData.js, move randomDocument function into helper functions

--- app.js
+++ app.js
@@ -58,8 +58,8 @@ app.use(passport.session());
 app.use(flash());
 
 // Landing Page Route
-const { getFormattedCurrentDate, getCurrentYear } = require('./controllers/services/helperFunctions');
-// const Translation = require('./models/Translation');
+const { getFormattedCurrentDate, getCurrentYear, getRandomDocuments } = require('./controllers/services/helperFunctions');
+const { sampleTranslations } = require('./controllers/services/sampleData')
 
 app.get('/', async (req, res) => {
   const date = getFormattedCurrentDate();
@@ -70,146 +70,7 @@ app.get('/', async (req, res) => {
   };
   console.log(message);
 
-  const sampleTranslations = [
-    {
-      _id: 1,
-      translation: 'Hej, jag heter Katelyn! Jag är en student från Florida. Jag ska vara i Sverige i tre månader; har ni några förslag på var jag ska besöka?',
-      sourceLanguage: 'English',
-      targetLanguage: 'Swedish',
-      formattedDate: 'May 21, 2023',
-      userDisplayName: 'Katelyn Gentry',
-      userImage: 'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-    },
-    {
-      _id: 2,
-      translation: 'Can you please help? I have run out of petrol. Where can I find the nearest petrol station for my car to refill?',
-      sourceLanguage: 'Finnish',
-      targetLanguage: 'English',
-      formattedDate: 'May 24, 2023',
-      userDisplayName: 'Eero Mäkinen',
-      userImage: 'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-    },
-    {
-      _id: 3,
-      translation: '食べ物はとてもおいしいです！ このレシピの秘密は何ですか？',
-      sourceLanguage: 'German',
-      targetLanguage: 'Japanese',
-      formattedDate: 'May 24, 2023',
-      userDisplayName: 'Julia Müller',
-      userImage: 'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-    },
-    {
-      _id: 4,
-      translation: 'الثقافة والتقاليد هنا لا تصدق. كانت عائلتك لطيفة بشكل لا يصدق أثناء إقامتنا هنا. شكرًا لك. أنت وعائلتك مدعوون إلى وطننا في أي وقت.',
-      sourceLanguage: 'Spanish',
-      targetLanguage: 'Arabic',
-      formattedDate: 'April 30, 2023',
-      userDisplayName: 'Ricardo Noll',
-      userImage: 'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80',
-    },
-    {
-      _id: 5,
-      translation: "Sto facendo un viaggio culturale in giro per l'Europa e ora sto attraversando l'Italia. Non vedo l'ora di visitare Napoli e assaggiare il cibo!",
-      sourceLanguage: 'English',
-      targetLanguage: 'Italian',
-      formattedDate: 'May 4, 2023',
-      userDisplayName: 'Anna Esposito',
-      userImage: 'https://images.unsplash.com/photo-1509783236416-c9ad59bae472?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80',
-    },
-    {
-      _id: 6,
-      translation: "나는 이것을 전혀 기대하지 않았습니다. 이곳 뉴욕에는 상당한 규모의 한국인이 존재합니다.",
-      sourceLanguage: 'English',
-      targetLanguage: 'Korean',
-      formattedDate: 'May 25, 2023',
-      userDisplayName: '민지 장 ',
-      userImage: 'https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80',
-    },
-    {
-      _id: 7,
-      translation: "Feliz aniversário vovó! Enviando muito amor da nossa família para você no Brasil. Espero vê-lo em breve.",
-      sourceLanguage: 'English',
-      targetLanguage: 'Portuguese',
-      formattedDate: 'May 24, 2023',
-      userDisplayName: 'Ben Richardson',
-      userImage: 'https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80',
-    },
-    {
-      _id: 8,
-      translation: "Avez-vous des indications pour l'aéroport? Notre vol part dans 2 heures et nous pourrions manquer notre vol si nous n'arrivons pas à temps.",
-      sourceLanguage: 'English',
-      targetLanguage: 'French',
-      formattedDate: 'May 29, 2023',
-      userDisplayName: 'Natalie Dubois',
-      userImage: 'https://images.unsplash.com/photo-1500917293891-ef795e70e1f6?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80',
-    },
-    {
-      _id: 9,
-      translation: "Сколько стоит керамическая кружка? У нас такое же мастерство дома, во Франции; выглядит феноменально!",
-      sourceLanguage: 'French',
-      targetLanguage: 'Russian',
-      formattedDate: 'May 27, 2023',
-      userDisplayName: 'Luis Johnson',
-      userImage: 'https://images.unsplash.com/photo-1463453091185-61582044d556?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80',
-    },
-    {
-      _id: 10,
-      translation: "Nie mogę się doczekać dzisiejszego meczu reprezentacji Polski! Czy wiemy o której zaczyna się mecz?",
-      sourceLanguage: 'English',
-      targetLanguage: 'Polish',
-      formattedDate: 'May 27, 2023',
-      userDisplayName: 'Wendy Armstrong',
-      userImage: 'https://images.unsplash.com/photo-1502685104226-ee32379fefbe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80',
-    },
-    {
-      _id: 11,
-      translation: "Our family is having a barbecue this weekend. We would love to invite you over to celebrate our grandmother's birthday.",
-      sourceLanguage: 'Spanish',
-      targetLanguage: 'English',
-      formattedDate: 'May 29, 2023',
-      userDisplayName: 'Sarah Stanton',
-      userImage: 'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80',
-    },
-    {
-      _id: 12,
-      translation: "¡Nos encantaría unirnos a tu barbacoa! ¿Qué hora es y cuántos años cumple tu abuela?",
-      sourceLanguage: 'English',
-      targetLanguage: 'Spanish',
-      formattedDate: 'May 29, 2023',
-      userDisplayName: 'Richard Stanton',
-      userImage: 'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80',
-    },
-    {
-      _id: 13,
-      translation: "हम तुमसे प्यार करते हैं। कृपया हमसे कभी भी मिलें, आप हमारे लिए परिवार हैं।",
-      sourceLanguage: 'English',
-      targetLanguage: 'Hindi',
-      formattedDate: 'May 29, 2023',
-      userDisplayName: 'Gabby Willmington',
-      userImage: 'https://images.unsplash.com/photo-1505840717430-882ce147ef2d?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80',
-    },
-    {
-      _id: 14,
-      translation: "Jag kan inte vänta med att besöka Stockholm. Har du några förslag på vad jag bör se, eller ställen jag bör äta?",
-      sourceLanguage: 'English',
-      targetLanguage: 'Swedish',
-      formattedDate: 'May 29, 2023',
-      userDisplayName: 'Rachel Flores',
-      userImage: 'https://images.unsplash.com/photo-1507101105822-7472b28e22ac?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80',
-    }
-  ];
-
-  function getRandomDocuments(array, size){
-    const shuffledArray = array.sort(() => 0.5 - Math.random())
-    return shuffledArray.slice(0, size)
-  }
-
   const translations = getRandomDocuments(sampleTranslations, 7)
-
-  // Select 7 random documents in Translation collection
-  // const sampleCollection = db.collection('sampleTranslations');
-  // const translations = await sampleCollection.aggregate([{ $sample: { size: 7 } }]).toArray();
-  // remove translations from render
 
   res.render('index.ejs', { date, currentYear: year, subscriptionKey, serviceRegion, translations, message, user: req.user });
 });
--- controllers/services/helperFunctions.js
+++ controllers/services/helperFunctions.js
@@ -1,55 +1,60 @@
 module.exports = {
-    getFormattedCurrentDate: () => {
-        const date = new Date(Date.now())
-        const format = { month: 'long', day: 'numeric', year: 'numeric', timeZone: 'America/New_York' }
-        return date.toLocaleDateString('en-US', format)        
-    },
+  getFormattedCurrentDate: () => {
+    const date = new Date(Date.now());
+    const format = { month: 'long', day: 'numeric', year: 'numeric', timeZone: 'America/New_York' };
+    return date.toLocaleDateString('en-US', format);
+  },
 
-    getCurrentYear: () => {
-        return new Date().getFullYear()
-    },
+  getCurrentYear: () => {
+    return new Date().getFullYear();
+  },
 
-    mapSourceLanguage: (languageCode) => {
-        const sourceLanguageMap = {
-            'en-US': 'English',
-            'de-DE': 'German',
-            'ar-EG': 'Arabic',
-            'es-ES': 'Spanish',
-            'fi-FI': 'Finnish',
-            'fr-FR': 'French',
-            'hi-IN': 'Hindi',
-            'it-IT': 'Italian',
-            'ja-JP': 'Japanese',
-            'ko-KR': 'Korean',
-            'pl-PL': 'Polish',
-            'pt-BR': 'Portugese',
-            'ru-RU': 'Russian',
-            'sv-SE': 'Swedish',
-            'zh-Hans': 'Chinese'
-        };
+  mapSourceLanguage: (languageCode) => {
+    const sourceLanguageMap = {
+      'en-US': 'English',
+      'de-DE': 'German',
+      'ar-EG': 'Arabic',
+      'es-ES': 'Spanish',
+      'fi-FI': 'Finnish',
+      'fr-FR': 'French',
+      'hi-IN': 'Hindi',
+      'it-IT': 'Italian',
+      'ja-JP': 'Japanese',
+      'ko-KR': 'Korean',
+      'pl-PL': 'Polish',
+      'pt-BR': 'Portugese',
+      'ru-RU': 'Russian',
+      'sv-SE': 'Swedish',
+      'zh-Hans': 'Chinese',
+    };
 
-        return sourceLanguageMap[languageCode]
-    },
+    return sourceLanguageMap[languageCode];
+  },
 
-    mapTargetLanguage: (languageCode) => {
-        const targetLanguageMap = {
-            'en': 'English',
-            'de': 'German',
-            'ar': 'Arabic',
-            'es': 'Spanish',
-            'fi': 'Finnish',
-            'fr': 'French',
-            'hi': 'Hindi',
-            'it': 'Italian',
-            'ja': 'Japanese',
-            'ko': 'Korean',
-            'pl': 'Polish',
-            'pt': 'Portugese',
-            'ru': 'Russian',
-            'sv': 'Swedish',
-            'zh-Hans': 'Chinese'
-        };
+  mapTargetLanguage: (languageCode) => {
+    const targetLanguageMap = {
+      en: 'English',
+      de: 'German',
+      ar: 'Arabic',
+      es: 'Spanish',
+      fi: 'Finnish',
+      fr: 'French',
+      hi: 'Hindi',
+      it: 'Italian',
+      ja: 'Japanese',
+      ko: 'Korean',
+      pl: 'Polish',
+      pt: 'Portugese',
+      ru: 'Russian',
+      sv: 'Swedish',
+      'zh-Hans': 'Chinese',
+    };
 
-        return targetLanguageMap[languageCode]
-    }
-}
+    return targetLanguageMap[languageCode];
+  },
+
+  getRandomDocuments: (array, size) => {
+    const shuffledArray = array.sort(() => 0.5 - Math.random());
+    return shuffledArray.slice(0, size);
+  },
+};
